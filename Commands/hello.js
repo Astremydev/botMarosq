a997c4aa7695b6689d82a3ea0e4a984d51c9ef02
Astremydev <44945270+Astremydev@users.noreply.github.com>
2018-11-11 09:36:13 -0500

    Add files via upload

---
+++ Commands/hello.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const botconfig = require("./botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+  message.delete();
+  message.channel.send("Bonjour à toi " + message.author.username + " !").then(msg => msg.delete(3000));
+}
+
+module.exports.help = {
+  name: "hello"
+}
